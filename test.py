3e8fbb3b8aa2b6478b82e2fa1c62b088674826d9
ira-sin <irapriyadarshini194@gmail.com>
2025-07-31 13:39:07 -0700

    Added fireworks

--- test.py
+++ test.py
@@ -1,3 +1,51 @@
+import time
+import random
+import os
+
 print("hello")
 for i in range(1, 11):
     print(i)
+
+fireworks = [
+    "        *        ",
+    "       ***       ",
+    "      *****      ",
+    "    *********    ",
+    "  *************  ",
+    "*****************",
+    "  *************  ",
+    "    *********    ",
+    "      *****      ",
+    "       ***       ",
+    "        *        "
+]
+
+colors = [
+    "\033[91m",  # Red
+    "\033[92m",  # Green
+    "\033[93m",  # Yellow
+    "\033[94m",  # Blue
+    "\033[95m",  # Magenta
+    "\033[96m",  # Cyan
+    "\033[97m",  # White
+]
+
+def clear():
+    os.system('clear')
+
+def show_firework():
+    color = random.choice(colors)
+    for line in fireworks:
+        print(color + line + "\033[0m")
+        time.sleep(0.05)
+    time.sleep(0.3)
+    clear()
+
+def fireworks_show(times=10):
+    clear()
+    for _ in range(times):
+        show_firework()
+    print("🎆 Happy Fireworks Show! 🎆")
+
+if __name__ == "__main__":
+    fireworks_show()
